5970dcab8dff31fd416eec63bf38d078289c47d8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-07 12:56:53 -0800

    added add_cubes

--- arithmetic.py
+++ arithmetic.py
@@ -53,3 +53,9 @@ def add_mult(num1, num2, num3):
     """Add first two and multiply sum with third."""
 
     return (num1 + num2) * num3
+
+
+def add_cubes(num1, num2):
+    """cube both numbers and sum them"""
+
+    return (num1 ** 3) + (num2 ** 3)
